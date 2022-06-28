12114f3981cb3c9bb7db96bd892e1e7718e8fff6
Kadir Karaduman <qadirkaraduman@gmail.com>
2022-06-28 11:33:57 +0300

    Ready For Heroku

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
 const express = require('express');
-const path = require('path');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require("method-override");
+const ejs = require('ejs');
+const { connect } = require('http2');
+const photoController = require("./controllers/photoControllers");
+const pageController = require("./controllers/pageControllers");
 
 const app = express();
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+//connect DB
+mongoose.connect('mongodb+srv://kadir:SToFol20R8h054TN@cluster0.vgtdkjx.mongodb.net/pcat-db?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-const port = 3000;
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLEWARES
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
+app.use(methodOverride("_method", {
+  methods: ["POST", "GET"]
+}));
+
+//ROUTES
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete("/photos/:id", photoController.deletePhoto)
+
+
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
+
+
+
+
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı..`);
 });
